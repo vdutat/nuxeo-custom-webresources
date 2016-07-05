8d1514aafd162f5f36aa9a10e0530afe14beb0ef
Vincent Dutat <vdutat@nuxeo.com>
2016-07-05 16:04:27 -0700

    comment console.log()

--- src/main/resources/web/nuxeo.war/scripts/richcalendar_disable_past_days.js
+++ src/main/resources/web/nuxeo.war/scripts/richcalendar_disable_past_days.js
@@ -1,6 +1,6 @@
 function richCalendarIsFutureDay(day) {
     var currentDate = new Date();
     rc = (currentDate.getTime() - day.date.getTime() < 0);
-    console.log('<richCalendarIsFutureDay> ' + day.date.toString() + ', ' + rc);
+    //console.log('<richCalendarIsFutureDay> ' + day.date.toString() + ', ' + rc);
     return rc;
 }
