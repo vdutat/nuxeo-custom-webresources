15c76ca022420fb4ebff66ebf79351352e7d7354
Vincent Dutat <vdutat@nuxeo.com>
2016-09-07 10:29:15 -0700

    simplify richcalendar_disable_past_days.js

--- src/main/resources/web/nuxeo.war/scripts/richcalendar_disable_past_days.js
+++ src/main/resources/web/nuxeo.war/scripts/richcalendar_disable_past_days.js
@@ -1,6 +1,25 @@
+function isDateInPast(day) {
+	var currentDate = new Date();
+	return (currentDate.getTime() - day.date.getTime() >= 0 && !isDateToday(day));
+}
+
+function isDateToday(day) {
+	var currentDate = new Date();
+	return (currentDate.getFullYear() == day.date.getFullYear() && currentDate.getMonth() == day.date.getMonth() && currentDate.getDate() == day.date.getDate());
+}
+
 function richCalendarIsFutureDay(day) {
-    var currentDate = new Date();
-    rc = (currentDate.getTime() - day.date.getTime() < 0);
-    //console.log('<richCalendarIsFutureDay> ' + day.date.toString() + ', ' + rc);
+    isInFuture = !isDateInPast(day);
+    //console.log('<richCalendarIsFutureDay> ' + day.date.toString() + ', ' + isInFuture);
+    return isInFuture;
+}
+
+function disabledClassesProv(day){
+    rc = '';
+    isInPast = isDateInPast(day);
+    if (isInPast) {
+    	rc = 'rf-cal-boundary-day';
+    }
+    //console.log('<disabledClassesProv> ' + day.date.toString() + ', ' + isInPast + ', ' + rc);
     return rc;
 }
